48911c9446b922e545f8b465175673e88dd9f995
matayuuu <65808410+matayuuu@users.noreply.github.com>
2021-11-15 09:59:32 +0900

    スプレット構文まで書いたよ

--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
-import "./styles.css";
+// const let
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var var1 = "変数";
+// console.log(var1);
+
+// let var2 = "let変数";
+// console.log(var2);
+
+// var2 = "let変数を上書き";
+// console.log(var2);
+
+// const var3 = "const変数";
+// console.log(var3);
+
+// const var4 = {
+//   name: "matayuuu",
+//   age: 27
+// };
+// var4.name = "matayuuu415"
+// console.log(var4);
+
+//テンプレート文字列
+// const name = "またゆう";
+// const age = 27;
+// const massage = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(massage);
+
+//アロー関数
+// function func1(str){
+//   return str
+// };
+//console.log(func1);
+// const funt1 = function (str){
+//   return str
+// }
+// console.log(funt1("aaa"));
+
+// const func2 = (str) => {
+//   return str
+// };
+// console.log(func2("func2"));
+
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 20));
+
+//分割代入(オブジェクト編)　※配列も可能
+// const myprofile = {
+//   name: "matayuuu",
+//   age: 27
+// };
+// console.log(`私の名前は${myprofile.name}です。年齢は${myprofile.age}です。`);
+
+// const { name, age } = myprofile;
+// console.log(`私の名前は${name}です。年齢は${age}です。`);
+
+//デフォルト値、引数など
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+// sayHello()
+// sayHello("またゆう")
+
+//スプレット構文
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+// const arr6 = [...arr4];
+// console.log(arr6);
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
